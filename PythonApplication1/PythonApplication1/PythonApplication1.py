164c65d6890370a61b144056828b2c221f02dff5
ahernandez67 <72721966+ahernandez67@users.noreply.github.com>
2023-04-21 15:41:18 -0700

    Delete PythonApplication1 directory

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++
@@ -1,8 +0,0 @@
-import pyodbc 
-cnxn = pyodbc.connect('DRIVER={Devart ODBC Driver for SQL Server};Server=myserver;Database=mydatabase;Trusted_Connection=yes')
-cursor = cnxn.cursor()	
-cursor.execute("SELECT * FROM EMP") 
-row = cursor.fetchone() 
-while row:
-    print (row) 
-    row = cursor.fetchone()
